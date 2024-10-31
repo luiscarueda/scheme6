e1657454a34bf6dcf51d43d0f885caca95c42341
luiscarlosrueda <luiscrueda.ing@gmail.com>
2024-10-31 18:03:19 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Footer, Header, Hero } from './sections'
+
+function App() {
+  
+
+  return (
+    <>
+       <Hero/>
+       <Header/>
+       <Footer/>
+    </>
+  )
+}
+
+export default App
